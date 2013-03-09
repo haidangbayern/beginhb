cfc4f2d2cb14e698599d5a58c23c74edea670520
haidangbayern <haidangbayern@yahoo.com.vn>
2013-03-09 19:41:02 +0700

    create project

---
+++ bundles/cms/language/vi/ajax_resp.php
@@ -0,0 +1,91 @@
+<?php
+
+return array(
+
+	"ownership_error" => "Bạn <strong>không đủ quyền</strong> để sửa item này.",
+
+	"page_settings_success" => "Cấu hình đã được lưu!",
+	"page_settings_error" => "Cấu hình có lỗi!",
+
+	"page_design_success" => "Thiết kế đã lưu!",
+	"page_design_error" => "Thiết kế có lỗi!",
+
+	"page_seo_success" => "SEO đã lưu!",
+	"page_seo_error" => "SEO có lỗi!",
+
+	"page_preview_success" => "Xem trước đã lưu!",
+	"page_preview_error" => "Xem trước có lỗi!",
+
+	"page_upload_success" => "Đã load",
+	"page_upload_error" => "Load trang bị lỗi!",
+	"page_upload_ftp" => "Upload tập tin này với FTP trong :path",
+
+	"element_success" => "Element đã lưu!",
+	"element_error" => "Element có lỗi!",
+
+	"role_save_success" => "Role đã lưu!",
+	"role_save_error" => "Role có lỗi!",
+
+	"user_account_success" => "User đã lưu!",
+	"user_account_error" => "User có lỗi!",
+
+	"user_password_success" => "Password đã lưu!",
+	"user_password_error" => "Password có lỗi!",
+	"user_nouser_error" => "User chưa được tạo!",
+
+	"user_details_success" => "User details đã lưu!",
+	"user_details_error" => "User details không lưu được!",
+
+	"filetext_text_success" => "Descriptions đã lưu!",
+	"filetext_text_error" => "Descriptions có lỗi!",
+
+	"filename_filename_success" => "File được sửa!",
+	"filename_filename_error" => "File chưa được sửa!",
+
+	"filename_fileavailable_success" => "File được nối!",
+	"filename_fileavailable_error" => "File chưa được nối!",
+
+	"translation_save_success" => "Translation set!",
+	"translation_save_error" => "Translation not set!",
+	"translation_delete_success" => "Translation đã được xóa!",
+	"translation_delete_error" => "Translation chưa được xóa!",
+
+	"menu_save_success" => "Menu đã lưu!",
+	"menu_save_error" => "Menu chưa lưu được!",
+
+	"page_relation_success" => "Pages đã nối!",
+	"page_relation_error" => "Pages chưa được nối!",
+
+	"banner_save_success" => "Banner được update!",
+	"banner_save_error" => "Banner chưa được update!",
+
+	"download_save_success" => "Download list updated!",
+	"download_save_error" => "Download list not updated!",
+
+	"gallery_save_success" => "Gallery updated!",
+	"gallery_save_error" => "Gallery not updated!",
+
+	"blog_post_success" => "Blog đã lưu!",
+	"blog_post_error" => "Blog không được lưu!",
+
+	"blog_preview_success" => "Preview đã lưu!",
+	"blog_preview_error" => "Preview không được lưu!",
+
+	"blog_available_success" => "Post đã kích hoạt!",
+	"blog_available_error" => "Post không được kích hoạt!",
+
+	"blog_relation_success" => "Post linked!",
+	"blog_relation_error" => "Post not linked!",
+
+	"blog_tags_success" => "Tag linked!",
+	"blog_tags_error" => "Tag not linked!",
+
+	"tag_save_success" => "Tag created!",
+	"tag_save_error" => "Tag not created!",
+
+	// CALENDAR
+
+	"calendar_success" => "Course saved!",
+	"calendar_error" => "Course not saved!",
+
+);
